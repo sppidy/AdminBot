71ff5e4660b744ff8d1c87380fc345465d10c48e
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-03-16 00:33:39 +0530

    Added Welcome

--- adminbot.py
+++ adminbot.py
@@ -26,6 +26,15 @@
 """
 from bot import *
 admin_cmd = cmd
+from telethon import events
+from telethon.utils import pack_bot_file_id
+from bot.sql.welcome_sql import (
+    add_welcome_setting,
+    get_current_welcome_settings,
+    rm_welcome_setting,
+    update_previous_welcome,
+)
+
 
 # =================== CONSTANT ===================
 PP_TOO_SMOL = "`The image is too small`"
@@ -431,24 +440,95 @@ async def get_user_sender_id(user, event):
     return user_obj
 
 @adminbot.on(events.ChatAction())
-@only_groups
 async def _(event):
-    if event.user_joined:
-        a_user = await event.get_user()
-        a_chat = await event.get_chat()
-        title = a_chat.title if a_chat.title else "this chat"
-        participants = await event.client.get_participants(chat)
-        count = len(participants)
-        mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
-        first = a_user.first_name
-        last = a_user.last_name
-        if last:
-            fullname = f"{first} {last}"
-        else:
-            fullname = first
-        userid = a_user.id
-        mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
-        await event.reply("Hi {mention},\nWelcome to {title}\nYou are {count}\nStay On-Topic\nHave Common Sense\n Have Safe Journey in Telegram With Ultroid",button=Button.url("Join Our Channel",url="https://t.me/TheUltroid"))
+    cws = get_current_welcome_settings(event.chat_id)
+    if cws:
+        # logger.info(event.stringify())
+        """user_added=False,
+        user_joined=True,
+        user_left=False,
+        user_kicked=False,"""
+        if event.user_joined:
+            if cws.should_clean_welcome:
+                try:
+                    await jarvis.delete_messages(  # pylint:disable=E0602
+                        event.chat_id, cws.previous_welcome
+                    )
+                except Exception as e:  # pylint:disable=C0103,W0703
+                    logger.warn(str(e))  # pylint:disable=E0602
+            a_user = await event.get_user()
+            chat = await event.get_chat()
+
+            title = chat.title if chat.title else "this chat"
+            participants = await event.client.get_participants(chat)
+            count = len(participants)
+            mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+            first = a_user.first_name
+            last = a_user.last_name
+            if last:
+                fullname = f"{first} {last}"
+            else:
+                fullname = first
+            userid = a_user.id
+            current_saved_welcome_message = cws.custom_welcome_message
+            mention = "[{}](tg://user?id={})".format(a_user.first_name, a_user.id)
+
+            current_message = await event.reply(
+                current_saved_welcome_message.format(
+                    mention=mention,
+                    title=title,
+                    count=count,
+                    first=first,
+                    last=last,
+                    fullname=fullname,
+                    username=username,
+                    userid=userid,
+                ),
+                file=cws.media_file_id,
+            )
+            update_previous_welcome(event.chat_id, current_message.id)
+
+
+@admin_cmd("savewelcome")  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    msg = await event.get_reply_message()
+    if msg and msg.media:
+        bot_api_file_id = pack_bot_file_id(msg.media)
+        add_welcome_setting(event.chat_id, msg.message, True, 0, bot_api_file_id)
+        await event.edit("Welcome note saved. ")
+    else:
+        input_str = event.text.split(None, 1)
+        add_welcome_setting(event.chat_id, input_str[1], True, 0, None)
+        await event.edit("Welcome note saved. ")
+
+
+@admin_cmd("clearwelcome")  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    cws = get_current_welcome_settings(event.chat_id)
+    rm_welcome_setting(event.chat_id)
+    await event.edit(
+        "Welcome note cleared. "
+        + "The previous welcome message was `{}`.".format(cws.custom_welcome_message)
+    )
+
+
+@admin_cmd("welcome")  # pylint:disable=E0602
+async def _(event):
+    if event.fwd_from:
+        return
+    cws = get_current_welcome_settings(event.chat_id)
+    if hasattr(cws, "custom_welcome_message"):
+        await event.edit(
+            "Welcome note found. "
+            + "Your welcome message is\n\n`{}`.".format(cws.custom_welcome_message)
+        )
+    else:
+        await event.edit("No Welcome Message found")
+
 
 print("Admin Bot Started !!")
 
