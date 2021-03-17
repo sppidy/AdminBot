d02f4494cce6b05f95ce9d249401837843d118b7
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-03-17 20:01:14 +0530

    Added Filters

--- adminbot.py
+++ adminbot.py
@@ -28,13 +28,8 @@ from bot import *
 admin_cmd = cmd
 from telethon import events
 from telethon.utils import pack_bot_file_id
-from bot.sql.welcomesql import (
-    add_welcome_setting,
-    get_current_welcome_settings,
-    rm_welcome_setting,
-    update_previous_welcome,
-)
-
+import bot.sql.welcomesql
+import bot.sql.filtersql
 
 # =================== CONSTANT ===================
 PP_TOO_SMOL = "`The image is too small`"
@@ -379,6 +374,9 @@ async def purge(event):
 
 
 @admin_cmd("del", is_args=False)
+@only_groups
+@is_bot_admin
+@is_admin
 async def delete_msg(event):
 
     chat = event.chat_id
@@ -489,7 +487,10 @@ async def _(event):
             update_previous_welcome(event.chat_id, current_message.id)
 
 
-@admin_cmd("savewelcome",is_args=True)  # pylint:disable=E0602
+@admin_cmd("savewelcome",is_args=True)
+@only_groups
+@is_bot_admin
+@is_admin
 async def _(event):
     if event.fwd_from:
         return
@@ -504,7 +505,10 @@ async def _(event):
         await event.reply("Welcome note saved. ")
 
 
-@admin_cmd("clearwelcome",is_args=True)  # pylint:disable=E0602
+@admin_cmd("clearwelcome",is_args=True)
+@only_groups
+@is_bot_admin
+@is_admin
 async def _(event):
     if event.fwd_from:
         return
@@ -516,7 +520,10 @@ async def _(event):
     )
 
 
-@admin_cmd("welcome")  # pylint:disable=E0602
+@admin_cmd("welcome")
+@only_groups
+@is_bot_admin
+@is_admin
 async def _(event):
     if event.fwd_from:
         return
@@ -529,6 +536,130 @@ async def _(event):
     else:
         await event.reply("No Welcome Message found")
 
+DELETE_TIMEOUT = 0
+TYPE_TEXT = 0
+TYPE_PHOTO = 1
+TYPE_DOCUMENT = 2
+
+
+global last_triggered_filters
+last_triggered_filters = {}  # pylint:disable=E0602
+
+
+@adminbot.on(events.NewMessage(incoming=True))
+async def on_snip(event):
+    global last_triggered_filters
+    name = event.raw_text
+    if event.chat_id in last_triggered_filters:
+        if name in last_triggered_filters[event.chat_id]:
+            return False
+    snips = get_all_filters(event.chat_id)
+    if snips:
+        for snip in snips:
+            pattern = r"( |^|[^\w])" + re.escape(snip.keyword) + r"( |$|[^\w])"
+            if re.search(pattern, name, flags=re.IGNORECASE):
+                if snip.snip_type == TYPE_PHOTO:
+                    media = types.InputPhoto(
+                        int(snip.media_id),
+                        int(snip.media_access_hash),
+                        snip.media_file_reference,
+                    )
+                elif snip.snip_type == TYPE_DOCUMENT:
+                    media = types.InputDocument(
+                        int(snip.media_id),
+                        int(snip.media_access_hash),
+                        snip.media_file_reference,
+                    )
+                else:
+                    media = None
+                event.message.id
+                if event.reply_to_msg_id:
+                    event.reply_to_msg_id
+                await event.reply(snip.reply, file=media)
+                if event.chat_id not in last_triggered_filters:
+                    last_triggered_filters[event.chat_id] = []
+                last_triggered_filters[event.chat_id].append(name)
+                await asyncio.sleep(DELETE_TIMEOUT)
+                last_triggered_filters[event.chat_id].remove(name)
+
+
+@admin_cmd("savefilter", is_args="simple")
+@only_groups
+@is_bot_admin
+@is_admin
+async def on_snip_save(event):
+    name = event.pattern_match.group(1)
+    msg = await event.get_reply_message()
+    if msg:
+        snip = {"type": TYPE_TEXT, "text": msg.message or ""}
+        if msg.media:
+            media = None
+            if isinstance(msg.media, types.MessageMediaPhoto):
+                media = utils.get_input_photo(msg.media.photo)
+                snip["type"] = TYPE_PHOTO
+            elif isinstance(msg.media, types.MessageMediaDocument):
+                media = utils.get_input_document(msg.media.document)
+                snip["type"] = TYPE_DOCUMENT
+            if media:
+                snip["id"] = media.id
+                snip["hash"] = media.access_hash
+                snip["fr"] = media.file_reference
+        add_filter(
+            event.chat_id,
+            name,
+            snip["text"],
+            snip["type"],
+            snip.get("id"),
+            snip.get("hash"),
+            snip.get("fr"),
+        )
+        await event.reply(f"filter {name} saved successfully. Get it with {name}")
+    else:
+        await event.reply("Reply to a message with `savefilter keyword` to save the filter")
+
+
+@admin_cmd("filters" ,is_args="simple")
+async def on_snip_list(event):
+    all_snips = get_all_filters(event.chat_id)
+    OUT_STR = "Available Filters in the Current Chat:\n"
+    if len(all_snips) > 0:
+        for a_snip in all_snips:
+            OUT_STR += f"👉 {a_snip.keyword} \n"
+    else:
+        OUT_STR = "No Filters. Start Saving using `.savefilter`"
+    if len(OUT_STR) > 4096:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "filters.text"
+            await jarvis.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="Available Filters in the Current Chat",
+                reply_to=event,
+            )
+            await event.delete()
+    else:
+        await event.reply(OUT_STR)
+
+
+@admin_cmd("clearfilter" is_args="simple")
+@only_groups
+@is_bot_admin
+@is_admin
+async def on_snip_delete(event):
+    name = event.pattern_match.group(1)
+    remove_filter(event.chat_id, name)
+    await event.reply(f"filter {name} deleted successfully")
+
+
+@admin_cmd("clearallfilters",is_args=False)
+@only_groups
+@is_bot_admin
+@is_admin
+async def on_all_snip_delete(event):
+    remove_all_filters(event.chat_id)
+    await event.reply(f"filters **in current chat** deleted successfully")
 
 print("Admin Bot Started !!")
 
