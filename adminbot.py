efddaeda5801c897a0d19c63c5ec61416e3d50bb
Sρι∂у <68327188+sppidy@users.noreply.github.com>
2021-03-19 17:53:11 +0530

    Help Menu Basics

--- adminbot.py
+++ adminbot.py
@@ -103,8 +103,11 @@ async def _(event):
 
 @callback("helpstarter")
 async def _(event):
-    await event.edit(f"{__doc__}",buttons=[Button.inline(" <-- Bᴀᴄᴋ", data="backer")])
+    await event.edit("Hey there! I am Administrator.\nI help admins manage their groups with Pro Features!\nHave a look at the following for an idea of some of the things I can help you with.\n\nMain commands available:\n • /help: PM's you this message.\n\nAll commands can either be used with / or !.\nAnd You May Check The Help for Plugins :",buttons=[Button.inline(" <-- Bᴀᴄᴋ", data="backer")])
 
+@callback("adminhelp")
+async def _(event):
+    await event.edit(f"{__doc__}",buttons=[Button.inline(" <-- Bᴀᴄᴋ", data="backer")])
 #==========================================================================
 
 @admin_cmd("start", is_args="simple")
