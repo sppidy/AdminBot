2b8120e212be5f463d555481cf0c28df2b320bb5
Sρι∂у <spidy@ultroid.tech>
2021-04-28 23:32:38 +0530

    Added Blacklist

--- adminbot.py
+++ adminbot.py
@@ -104,22 +104,25 @@ async def _(event):
 		    [
 			    Button.inline("Antiflood", data="antifloodhelp"),
 			    Button.inline("Bans", data="banhelp"),
-			    Button.inline("Promote" , data="promotehelp"),
+			    Button.inline("Blacklists" , data="blacklisthelp"),
 		    ],
 		    [
+			    Button.inline("Promote" , data="promotehelp"),
 			    Button.inline("Kick" , data="kickhelp"),
 			    Button.inline("Pin/Unpin", data="pinhelp"),
-			    Button.inline("Purge" , data="purgehelp"),
 		    ],
 		    [
+			    Button.inline("Purge" , data="purgehelp"),
 			    Button.inline("Filters" , data="filterhelp"),
 			    Button.inline("Rules" , data="ruleshelp"),
-			    Button.inline("Locks", data="lockhelp"),
 		    ],
 		    [
+			    Button.inline("Locks", data="lockhelp"),
 			    Button.inline("Notes", data="noteshelp"),
 			    Button.inline("Warns", data="warnhelp"),
-			    Button.inline("Welcome" , data="welcomehelp"),
+		    ],
+		    [
+			    Button.inline("Welcome / Goodbye" , data="welcomehelp"),
 		    ],
 		    [
 			    Button.inline("<-- Back" , data="backer"),
@@ -1826,6 +1829,279 @@ Examples of time value: 4m = 4 minutes, 3h = 3 hours, 6d = 6 days, 5w = 5 weeks.
             )
     except Exception as e:
         print(e)
+#======================================================================
+#Blacklist
+
+import os
+import re
+from telethon import events
+import bot.sql.blacklistbsql as bsql
+import bot.sql.urlblacklistbsql as urlsql
+from julia.events import register
+from telethon import types
+from telethon.tl import functions
+import html
+import tldextract
+
+
+async def can_change_info(message):
+    result = await adminbot(
+        functions.channels.GetParticipantRequest(
+            channel=message.chat_id,
+            user_id=message.sender_id,
+        )
+    )
+    p = result.participant
+    return isinstance(p, types.ChannelParticipantCreator) or (
+        isinstance(p, types.ChannelParticipantAdmin) and p.admin_rights.change_info
+    )
+
+
+async def is_register_admin(chat, user):
+    if isinstance(chat, (types.InputPeerChannel, types.InputChannel)):
+        return isinstance(
+            (
+                await adminbot(functions.channels.GetParticipantRequest(chat, user))
+            ).participant,
+            (types.ChannelParticipantAdmin, types.ChannelParticipantCreator),
+        )
+    if isinstance(chat, types.InputPeerUser):
+        return True
+
+
+@adminbot.on(events.NewMessage(incoming=True))
+async def on_new_message(event):
+    # TODO: exempt admins from locks
+    if event.is_private:
+        return
+    if await is_register_admin(event.input_chat, event.message.sender_id):
+        return
+
+    name = event.text
+    snips = bsql.get_chat_blacklist(event.chat_id)
+    for snip in snips:
+        pattern = r"( |^|[^\w])" + re.escape(snip) + r"( |$|[^\w])"
+        # print(re.search(pattern, name, flags=re.IGNORECASE))
+        if re.search(pattern, name, flags=re.IGNORECASE):
+            try:
+                await event.delete()
+            except Exception as e:
+                print(e)
+
+
+@admin_cmd("addblacklist", is_args="blacklist")
+async def on_add_black_list(event):
+    if event.is_group:
+        if not await can_change_info(message=event):
+            return
+    else:
+        return
+    text = event.pattern_match.group(1)
+    to_blacklist = list(
+        {trigger.strip() for trigger in text.split("\n") if trigger.strip()}
+    )
+    for trigger in to_blacklist:
+        bsql.add_to_blacklist(event.chat_id, trigger.lower())
+    await event.reply(
+        "Added {} triggers to the blacklist in the current chat".format(
+            len(to_blacklist)
+        )
+    )
+
+
+@admin_cmd("listblacklist", is_args=False)
+async def on_view_blacklist(event):
+    all_blacklisted = bsql.get_chat_blacklist(event.chat_id)
+    OUT_STR = "**Blacklists in the Current Chat:\n**"
+    if len(all_blacklisted) > 0:
+        for trigger in all_blacklisted:
+            OUT_STR += f"👉 {trigger} \n"
+    else:
+        OUT_STR = "No BlackLists. Start Adding using /addblacklist"
+    if len(OUT_STR) > 4096:
+        with io.BytesIO(str.encode(OUT_STR)) as out_file:
+            out_file.name = "blacklist.text"
+            await adminbot.send_file(
+                event.chat_id,
+                out_file,
+                force_document=True,
+                allow_cache=False,
+                caption="BlackLists in the Current Chat",
+                reply_to=event,
+            )
+            await event.delete()
+    else:
+        await event.reply(OUT_STR)
+
+
+@admin_cmd("rmblacklist", is_args="blacklist")
+async def on_delete_blacklist(event):
+    if event.is_group:
+        if not await can_change_info(message=event):
+            return
+    else:
+        return
+    text = event.pattern_match.group(1)
+    to_unblacklist = list(
+        {trigger.strip() for trigger in text.split("\n") if trigger.strip()}
+    )
+    successful = 0
+    for trigger in to_unblacklist:
+        if bsql.rm_from_blacklist(event.chat_id, trigger.lower()):
+            successful += 1
+    await event.reply(
+        f"Removed {successful} / {len(to_unblacklist)} from the blacklist"
+    )
+
+
+@admin_cmd("addurl", is_args="normal")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_private:
+        return
+    if event.is_group:
+        if await can_change_info(message=event):
+            pass
+        else:
+            return
+    chat = event.chat
+    urls = event.text.split(None, 1)
+    if len(urls) > 1:
+        urls = urls[1]
+        to_blacklist = list({uri.strip() for uri in urls.split("\n") if uri.strip()})
+        blacklisted = []
+
+        for uri in to_blacklist:
+            extract_url = tldextract.extract(uri)
+            if extract_url.domain and extract_url.suffix:
+                blacklisted.append(extract_url.domain + "." + extract_url.suffix)
+                urlsql.blacklist_url(
+                    chat.id, extract_url.domain + "." + extract_url.suffix
+                )
+
+        if len(to_blacklist) == 1:
+            extract_url = tldextract.extract(to_blacklist[0])
+            if extract_url.domain and extract_url.suffix:
+                await event.reply(
+                    "Added <code>{}</code> domain to the blacklist!".format(
+                        html.escape(extract_url.domain + "." + extract_url.suffix)
+                    ),
+                    parse_mode="html",
+                )
+            else:
+                await event.reply("You are trying to blacklist an invalid url")
+        else:
+            await event.reply(
+                "Added <code>{}</code> domains to the blacklist.".format(
+                    len(blacklisted)
+                ),
+                parse_mode="html",
+            )
+    else:
+        await event.reply("Tell me which urls you would like to add to the blacklist.")
+
+
+@admin_cmd("delurl", is_args="normal")
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_private:
+        return
+    if event.is_group:
+        if await can_change_info(message=event):
+            pass
+        else:
+            return
+    chat = event.chat
+    urls = event.text.split(None, 1)
+
+    if len(urls) > 1:
+        urls = urls[1]
+        to_unblacklist = list({uri.strip() for uri in urls.split("\n") if uri.strip()})
+        unblacklisted = 0
+        for uri in to_unblacklist:
+            extract_url = tldextract.extract(uri)
+            success = urlsql.rm_url_from_blacklist(
+                chat.id, extract_url.domain + "." + extract_url.suffix
+            )
+            if success:
+                unblacklisted += 1
+
+        if len(to_unblacklist) == 1:
+            if unblacklisted:
+                await event.reply(
+                    "Removed <code>{}</code> from the blacklist!".format(
+                        html.escape(to_unblacklist[0])
+                    ),
+                    parse_mode="html",
+                )
+            else:
+                await event.reply("This isn't a blacklisted domain...!")
+        elif unblacklisted == len(to_unblacklist):
+            await event.reply(
+                "Removed <code>{}</code> domains from the blacklist.".format(
+                    unblacklisted
+                ),
+                parse_mode="html",
+            )
+        elif not unblacklisted:
+            await event.reply("None of these domains exist, so they weren't removed.")
+        else:
+            await event.reply(
+                "Removed <code>{}</code> domains from the blacklist. {} did not exist, so were not removed.".format(
+                    unblacklisted, len(to_unblacklist) - unblacklisted
+                ),
+                parse_mode="html",
+            )
+    else:
+        await event.reply(
+            "Tell me which domains you would like to remove from the blacklist."
+        )
+
+
+@adminbot.on(events.NewMessage(incoming=True))
+async def on_url_message(event):
+    if event.is_private:
+        return
+    chat = event.chat
+    extracted_domains = []
+    for (ent, txt) in event.get_entities_text():
+        if ent.offset != 0:
+            break
+        if isinstance(ent, types.MessageEntityUrl):
+            url = txt
+            extract_url = tldextract.extract(url)
+            extracted_domains.append(extract_url.domain + "." + extract_url.suffix)
+    for url in urlsql.get_blacklisted_urls(chat.id):
+        if url in extracted_domains:
+            try:
+                await event.delete()
+            except:
+                return
+
+
+@admin_cmd("geturl", is_args=False)
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.is_private:
+        return
+    if event.is_group:
+        if await can_change_info(message=event):
+            pass
+        else:
+            return
+    chat = event.chat
+    base_string = "Current <b>blacklisted</b> domains:\n"
+    blacklisted = urlsql.get_blacklisted_urls(chat.id)
+    if not blacklisted:
+        await event.reply("There are no blacklisted domains here!")
+        return
+    for domain in blacklisted:
+        base_string += "- <code>{}</code>\n".format(domain)
+    await event.reply(base_string, parse_mode="html")
+
 
 #======================================================================
 print("Admin Bot Started !!")
